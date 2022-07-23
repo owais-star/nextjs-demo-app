12a81dfe25e89731e7a3f61464a32566df008aee
owais-star <iqbalowais35@gmail.com>
2022-07-24 00:38:51 +0500

    changed baseUrl to remote url

--- constants/ApiUrl.js
+++ constants/ApiUrl.js
@@ -1,3 +1,4 @@
 // const dev = "http://localhost:5000"
 // export const baseUrl = window.location.hostname.split(":")[0] === "localhost" ? dev : "";
-export const baseUrl = "http://localhost:5000"
+// export const baseUrl = "http://localhost:5000"
+export const baseUrl = "https://cricketlive1.herokuapp.com"
